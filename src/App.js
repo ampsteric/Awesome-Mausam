f145d467de781f48fba5be58bcf926283b8aeeec
ampsteric <ampsteric@gmail.com>
2021-04-12 12:08:04 +0530

    show current weather

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useState } from "react";
 import "./App.css";
-
+import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 function App() {
-    return <div className="App"></div>;
+    const [City, Setcity] = useState("Delhi");
+    return (
+        <div className="App">
+            <input
+                onChange={(e) => {
+                    Setcity(e.target.value);
+                }}
+            />
+            <CurrentWeather city={City} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,10 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import dotenv from "dotenv";
 
-export default function CurrentWeather() {
+export default function CurrentWeather(props) {
+    const [Weather, setweather] = useState({});
     useEffect(() => {
-        axios.get(
-            `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-        );
-    }, []);
-    return <div></div>;
+        axios
+            .get(
+                `http://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+                // `http://api.openweathermap.org/data/2.5/weather?q=delhi&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+            )
+            .then((res) => {
+                setweather(res.data);
+            });
+    }, [props.city]);
+    console.log(Weather);
+
+    var unixtime = Weather.dt;
+    var s = new Date(unixtime * 1000).toLocaleDateString("en-US");
+
+    return (
+        <div>
+            {Object.keys(Weather).length === 0 &&
+            Weather.constructor === Object ? (
+                <div></div>
+            ) : (
+                <div>
+                    <h2>Name : {Weather.name}</h2>
+                    <h2>Country : {Weather.sys.country}</h2>
+                    <h2>Latitude : {Weather.coord.lat}</h2>
+                    <h2>Longitude : {Weather.coord.lon}</h2>
+                    <h2>Weather : {Weather.weather[0].main}</h2>
+                    <h2>Temp : {Weather.main.temp}</h2>
+                    <h2>pressure : {Weather.main.pressure}</h2>
+                    <h2>Humidity : {Weather.main.humidity}</h2>
+                    <h2>visibility : {Weather.visibility}</h2>
+                    <h2>Wind : {Weather.wind.speed}</h2>
+                    <h2>Date : {s}</h2>
+                </div>
+            )}
+        </div>
+    );
 }
